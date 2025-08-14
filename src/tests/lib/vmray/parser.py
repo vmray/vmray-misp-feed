de6e3d63bf84d051adb9e6561730ebcee75cda93
Jens Thom <jthom@vmray.com>
2025-08-14 12:52:00 +0200

    add test for SampleReport class

--- src/tests/lib/vmray/parser.py
+++ src/tests/lib/vmray/parser.py
@@ -4,7 +4,9 @@ import json
 
 import pytest
 
-from lib.vmray.parser import VMRayParser
+from lib.config import Config
+from lib.vmray.api_wrapper import VMRay as VMRayAPI
+from lib.vmray.parser import SampleReport, VMRayParser
 
 
 def remove_changing_values(event):
@@ -50,3 +52,15 @@ def test_parser(parser, expected_event):
     event_json = remove_changing_values(event_json)
 
     assert event_json == expected_event
+
+
+def test_sample_report(config: Config):
+    api_conf = config.vmray
+    mocked_api = VMRayAPI(
+        api_conf.host, api_conf.api_key, api_conf.verify_cert, api_conf.chunk_size
+    )
+    report = SampleReport(123, mocked_api)
+
+    assert len(list(report.artifacts())) == 6
+    assert len(list(report.mitre_attacks())) == 7
+    assert len(list(report.vtis())) == 11
